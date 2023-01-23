76d293369c4adbc4cc6e6a8f27972ad66764bc33
quan-p <m.phamquan@gmail.com>
2023-01-23 17:10:41 -0600

    Create placeholder page

---
+++ src/pages/Post.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+const Post = () => {
+    
+    return (
+        <div>
+            This is the Post page
+        </div>
+    );
+}
+
+export default Post;
