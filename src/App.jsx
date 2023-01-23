55141ac7489ea7e9f8e1d62dabd1f0043adf1436
quan-p <m.phamquan@gmail.com>
2023-01-23 17:11:31 -0600

    Create two routes and add link to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Nav from './components/nav/Nav';
 import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import Post from './pages/Post';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Home />
+      <Routes>
+        <Route path='/' element={ <Home/> } />
+        <Route path='posts' element={ <Post/> } />
+      </Routes>
+      
     </div>
   )
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Nav.scss';
 
 const Nav = () => {
     return (
         <div className="nav-container">
             This is a navbar
+            <Link to='/'>Home</Link>
+            <Link to='posts'>Posts</Link>
         </div>
     )
 }
