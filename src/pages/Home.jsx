09c3f099c92aad2f7525321a6bed44a12dc359d8
quan-p <m.phamquan@gmail.com>
2023-01-23 16:12:31 -0600

    Sort posts fetch and create state for 3 most recent posts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from "react";
 
 export default function Home() {
     const [posts, setPosts] = useState();
-    //const [recentPosts, setRecentPosts] = useState();
+    const [recentPosts, setRecentPosts] = useState();
     useEffect(() => {
         const fetchPosts = async () => {
             try {
                 const req = await fetch('https://blog-api-ifcw.onrender.com/posts');
                 const reqJson = await req.json();
                 setPosts(reqJson.posts);
-                // const sortPosts = posts.sort((a, b) =>  new Date(b.date) - new Date(a.date));
-                // setRecentPosts(sortPosts[0], sortPosts[1], sortPosts[2]);
+                const sortPosts = reqJson.posts.sort((a, b) =>  new Date(b.date) - new Date(a.date));
+                setRecentPosts(sortPosts.slice(0, 3));
             } catch (err) {
                 console.log(err);
             }
@@ -19,14 +19,15 @@ export default function Home() {
     }, []); 
     return (
         <div className="home-container">
-            {posts ?
+            {posts && recentPosts ?
                 <div className="post-container">
-                    <h3>Title</h3>
+                    <h3>Recent Posts</h3>
                     <ul>
-                        {posts.map(post => {
+                        {recentPosts.map(post => {
                             return <li key={post._id}>{post.title}</li>
                         })}
                     </ul>
+                    
                 </div> 
                 : <div>Nothing is loaded</div>
             }
