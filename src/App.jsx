479dccadf366b5be51439b30a6967215ddf781de
quan-p <m.phamquan@gmail.com>
2023-01-23 16:53:30 -0600

    Create navbar placeholder and position at top of page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Nav from './components/nav/Nav';
 import Home from './pages/Home';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Home />
     </div>
   )
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Nav.scss';
+
+const Nav = () => {
+    return (
+        <div className="nav-container">
+            This is a navbar
+        </div>
+    )
+}
+
+export default Nav;
