52ecc2166f55753f16ad4cba20bf056156fec37b
quan-p <m.phamquan@gmail.com>
2023-01-24 15:32:47 -0600

    Add footer component to bottom

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Nav from './components/nav/Nav';
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import Post from './pages/Post';
+import Footer from './components/footer/Footer';
 import './App.scss';
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Route path='/' element={ <Home/> } />
         <Route path='posts' element={ <Post/> } />
       </Routes>
-      
+      <Footer />
     </div>
   )
 }
