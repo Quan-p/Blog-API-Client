b69010dc2a5c8c06306bfae76b6997defbccc4c9
quan-p <m.phamquan@gmail.com>
2023-01-24 17:33:32 -0600

    Lift the fetch method for posts up to the App component in order to minimize calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import Nav from './components/nav/Nav';
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
@@ -6,12 +7,29 @@ import Footer from './components/footer/Footer';
 import './App.scss';
 
 function App() {
+  const [posts, setPosts] = useState();
+  //const [recentPosts, setRecentPosts] = useState();
+  useEffect(() => {
+      const fetchPosts = async () => {
+          try {
+              const req = await fetch('https://blog-api-ifcw.onrender.com/posts');
+              const reqJson = await req.json();
+              setPosts(reqJson.posts);
+              // const sortPosts = reqJson.posts.sort((a, b) =>  new Date(b.date) - new Date(a.date));
+              // setRecentPosts(sortPosts.slice(0, 3));
+          } catch (err) {
+              console.log(err);
+          }
+      }
+      fetchPosts();
+  }, []); 
+  
   return (
     <div className="App">
       <Nav />
       <Routes>
-        <Route path='/' element={ <Home/> } />
-        <Route path='posts' element={ <Post/> } />
+        <Route path='/' element={ <Home posts={posts} /> } />
+        <Route path='posts' element={ <Post posts={posts} /> } />
       </Routes>
       <Footer />
     </div>
