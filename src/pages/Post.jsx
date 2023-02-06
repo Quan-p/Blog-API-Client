928e5873766768af8ca38f01bdcafcfc2741ff15
quan-p <m.phamquan@gmail.com>
2023-02-06 15:29:00 -0600

    Create sorted posts by date and map entire list to show on Posts page

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,10 +1,27 @@
 import React, { useEffect, useState } from "react";
 
-const Post = () => {
-    
+const Post = (props) => {
+    const posts = props.posts;
+    const [sortedPosts, setSortedPosts] = useState();
+
+    useEffect(() => {
+        if(posts) {
+            const sortPosts = posts.sort((a, b) =>  new Date(b.date) - new Date(a.date));
+            setSortedPosts(sortPosts);
+        }
+    }, [posts]);
+
     return (
         <div>
             This is the Post page
+            {sortedPosts ?
+                <ul>
+                {sortedPosts.map(post => {
+                    return <li key={post._id}>{post.title}</li>
+                })}
+                </ul>
+                : <div>Nothing is loaded</div>
+            }
         </div>
     );
 }
