d0ac69589ead7efa1bab15b4e63547d7879dd3ea
quan-p <m.phamquan@gmail.com>
2023-02-13 16:54:03 -0600

    Check if post id matched with a userid and then extracted the username from that to render for each post

--- src/components/post_card/PostCard.jsx
+++ src/components/post_card/PostCard.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function PostCard({ post }) {
+function PostCard({ post, users }) {
     const title = post.title;
     const date = post.date;
+    let author = users.find(user => user._id == post.author);
 
     return (
         <a href={`/posts/${post._id}`} key={post._id}>
             <div>
-                <h1>{post.author}</h1>
+                <h1>{author.username}</h1>
                 <h2>{title}</h2>
                 <p>{date}</p>
             </div>
