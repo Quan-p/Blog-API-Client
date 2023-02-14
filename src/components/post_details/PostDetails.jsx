8921571298c91f408cf7d530e1296dca6042d70f
quan-p <m.phamquan@gmail.com>
2023-02-14 16:48:11 -0600

    Get post id from the URL and use find method to render post details

--- src/components/post_details/PostDetails.jsx
+++ src/components/post_details/PostDetails.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 
-const PostDetails = (posts) => {
+const PostDetails = (props) => {
+    if (!props || !props.posts) {
+        return null;
+    }
+
+    const { posts, users, handleGetAuthorUsername } = props;
+
+    const postId = window.location.pathname.split("/").pop();
+    const postsArray = Object.values(posts).flat();
+    let postObj = postsArray.find(post => post._id == postId);
+    const authorUsername = handleGetAuthorUsername(postObj.author)
+    console.log(authorUsername);
+    // if (!postObj) {
+    //     return <div>Post not found</div>;
+    // }
+
     return (
         <div>
             This is the post details page
+            <div>{postObj.title}</div>
+            <div>{authorUsername}</div>
         </div>
     )
 }
