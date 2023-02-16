99f21d500206f753bfed28228bf03a03d9fc6e0e
quan-p <m.phamquan@gmail.com>
2023-02-16 17:24:38 -0600

    Create several checks for the existence of postObj before calling the handleGetAuthorUsername

--- src/components/post_details/PostDetails.jsx
+++ src/components/post_details/PostDetails.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 
 const PostDetails = (props) => {
-    if (!props || !props.posts) {
-        return null;
-    }
-
     const { posts, users, handleGetAuthorUsername } = props;
 
+    if (!posts || !users || !handleGetAuthorUsername) {
+        return <div>Loading...</div>;
+    }
+
     const postId = window.location.pathname.split("/").pop();
-    const postsArray = Object.values(posts).flat();
-    let postObj = postsArray.find(post => post._id == postId);
-    const authorUsername = handleGetAuthorUsername(postObj.author)
-    console.log(authorUsername);
-    // if (!postObj) {
-    //     return <div>Post not found</div>;
-    // }
+    let postsArray = Array.from(posts ? Object.values(posts): []);
+    const flatArray = postsArray.flat();
+    let postObj = flatArray.find(post => post._id == postId);
+    
+    
+    let authorUsername = postObj?.author && handleGetAuthorUsername(postObj.author);
+    
+    if (!postObj) {
+        return <div>Post not found</div>;
+    }
 
     return (
         <div>
             This is the post details page
-            <div>{postObj.title}</div>
-            <div>{authorUsername}</div>
+            {postObj && authorUsername ? (
+                <div>
+                    <div>{postObj.title}</div>
+                    <div>{authorUsername}</div>
+                </div>
+            ): <div>Loading...</div>}
         </div>
     )
 }
