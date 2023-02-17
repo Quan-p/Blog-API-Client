38b1151b6a04b6572088aebe7d9037d3c3bf9c97
quan-p <m.phamquan@gmail.com>
2023-02-17 15:17:36 -0600

    Remove placeholders for hero component and add more text with a button

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,7 +3,16 @@ import React from "react";
 const Hero = () => {
     return (
         <div>
-            Hero Section
+            <h2>Welcome to my Blog</h2>
+            <p>
+                Created as a project for The Odin Project. 
+                Check out my random thoughts and amazing test posts. 
+            </p>
+            <button>
+                <a href={'/posts'}>
+                    Start Reading
+                </a>
+            </button>
         </div>
     )
 }
