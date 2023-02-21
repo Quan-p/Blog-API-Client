c4985f6decfec093264b45b8960394d075ce7e5b
quan-p <m.phamquan@gmail.com>
2023-02-21 15:46:52 -0600

    Set classnames

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import './hero.scss';
 
 const Hero = () => {
     return (
-        <div>
-            <h2>Welcome to my Blog</h2>
-            <p>
+        <div className="hero-container">
+            <h2 className="hero-title">Welcome to my Blog</h2>
+            <h3 className="hero-subtitle">
                 Created as a project for The Odin Project. 
                 Check out my random thoughts and amazing test posts. 
-            </p>
-            <button>
+            </h3>
+            <button className="hero-button">
                 <a href={'/posts'}>
                     Start Reading
                 </a>
