79f8300f6ec314e464b98ff29cb25890e4b922ec
quan-p <m.phamquan@gmail.com>
2023-03-01 15:33:46 -0600

    Fetch comments based on postId and set that to commentArray to be mapped and rendered

--- src/components/post_details/PostDetails.jsx
+++ src/components/post_details/PostDetails.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ClimbingBoxLoader } from "react-spinners";
 import './postDetails.scss';
 
 const PostDetails = (props) => {
     const { posts, users, handleGetAuthorUsername } = props;
+    const [commentArray, setCommentArray] = useState();
 
+    const postId = window.location.pathname.split("/").pop();
+
+    useEffect(() => {
+        const fetchComments = async() => {
+            try {
+                const commentReq = await fetch(`https://blog-api-ifcw.onrender.com/posts/${postId}/comments`);
+                const commentJson = await commentReq.json();
+                console.log(commentJson[0]);
+                if (Array.isArray(commentJson)) {
+                    console.log(commentJson.length);
+                  } else {
+                    console.log('commentJson is not an array');
+                    console.dir(commentJson);
+                    console.log(commentJson.comments[1].text)
+                }
+                setCommentArray(commentJson.comments);
+            } catch(error) {
+                console.error(error)
+            }
+        };
+        fetchComments();
+    }, [postId]);
     const override = {
         position: 'static',
         display: 'flex',
@@ -20,7 +43,7 @@ const PostDetails = (props) => {
                 />;
     }
 
-    const postId = window.location.pathname.split("/").pop();
+    
     let postsArray = Array.from(posts ? Object.values(posts): []);
     const flatArray = postsArray.flat();
     let postObj = flatArray.find(post => post._id == postId);
@@ -46,9 +69,13 @@ const PostDetails = (props) => {
                     <h3>{date}</h3>
                     <div className="content-container">{postObj.content}</div>
                     <ul className="comment-list">
-                        {postObj.comments.map(post => {
-                            return <li key={post._id}>{post}</li>
-                        })}
+                        {commentArray.length > 0 ? (
+                            commentArray.map(comment => {
+                                return <li key={comment._id}>{comment.text}</li>
+                            })
+                        ) : (
+                            <p>No comments yet</p>
+                        )}
                     </ul>
                 </div>
             ): <ClimbingBoxLoader
@@ -57,6 +84,7 @@ const PostDetails = (props) => {
                     cssOverride={ override }
                 />
             }
+            
         </div>
     )
 }
