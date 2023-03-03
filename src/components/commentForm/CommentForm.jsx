6bd161b8f0ba44d2da0487d0f22b653065451ec9
quan-p <m.phamquan@gmail.com>
2023-03-02 18:43:06 -0600

    Create comment form component with no functional post call

---
+++ src/components/commentForm/CommentForm.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+
+const CommentForm = (postId) => {
+    const [user, setUser] = useState('');
+    const [text, setText] = useState('');
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const postCall = await fetch(`https://blog-api-ifcw.onrender.com/posts/${postId}/comment`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ user, text }),
+            });
+            if (res.ok) {
+                console.log('Comment submitted successfully!');
+                setUser('');
+                setText('');
+              } else {
+                throw new Error('Form submission failed!');
+              }
+        } catch (error) { 
+            console.error(error);
+        }
+    }
+
+    return (
+        <div>
+            <h3>Comment Form</h3>
+            <form>
+                <label>
+                    Username:
+                    <input type="text" value={user} onChange={(e) => setUser(e.target.value)} />
+                </label>
+                    <label>
+                        Message:
+                        <textarea value={text} onChange={(e) => setText(e.target.value)} />
+                    </label>
+                    <button type="submit">
+                        Submit
+                    </button>
+                    
+            </form>
+        </div>
+    )
+}
+
+export default CommentForm;
