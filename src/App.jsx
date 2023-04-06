45c228d2e81c3a0bf6bb20d74d181a7805df0246
quan-p <m.phamquan@gmail.com>
2023-04-06 17:54:07 -0500

    Filter all posts fetched for if their published status is set to true

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,82 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import Nav from './components/nav/Nav';
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import Post from './pages/Post';
 import Footer from './components/footer/Footer';
 import './App.scss';
-import PostDetails from "./components/post_details/PostDetails";
+import PostDetails from './components/post_details/PostDetails';
 
 function App() {
-  const [posts, setPosts] = useState();
-  const [users, setUsers] = useState();
+	const [posts, setPosts] = useState();
+	const [users, setUsers] = useState();
+	const [publishedPosts, setPublishedPosts] = useState();
 
-  useEffect(() => {
-      const fetchData = async () => {
-          try {
-              const postReq = await fetch('https://blog-api-ifcw.onrender.com/posts');
-              const postsJson = await postReq.json();
-              setPosts(postsJson.posts);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const postReq = await fetch(
+					'https://blog-api-ifcw.onrender.com/posts'
+				);
+				const postsJson = await postReq.json();
+				setPosts(postsJson.posts);
 
-              const userReq = await fetch('https://blog-api-ifcw.onrender.com/users');
-              const userJson = await userReq.json();
-              setUsers(userJson.users);
-          } catch (err) {
-              console.log(err);
-          }
-      }
-      fetchData();
-  }, []); 
-  
-  const handleGetAuthorUsername = (authorId) => {
-    const author = users.find(user => user._id == authorId);
-    return author ? author.username : null;
-  }
+				const userReq = await fetch(
+					'https://blog-api-ifcw.onrender.com/users'
+				);
+				const userJson = await userReq.json();
+				setUsers(userJson.users);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+		fetchData();
+	}, []);
 
-  return (
-    <div className="App">
-      <Nav />
-      <main>
-        <Routes>
-          <Route path='/' element={ <Home posts={posts} users={users}/> } />
-          <Route path='posts' element={ <Post posts={posts} /> } />
-          <Route path='/posts/:postid' element={ <PostDetails posts={posts} users={users} handleGetAuthorUsername={handleGetAuthorUsername} /> } />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
-  )
+	useEffect(() => {
+		if (posts) {
+			setPublishedPosts(
+				posts.filter((post) => {
+					if (post.published) {
+						return post;
+					}
+				})
+			);
+		}
+	}, [posts]);
+
+	const handleGetAuthorUsername = (authorId) => {
+		const author = users.find((user) => user._id == authorId);
+		return author ? author.username : null;
+	};
+
+	return (
+		<div className='App'>
+			<Nav />
+			<main>
+				<Routes>
+					<Route
+						path='/'
+						element={<Home posts={posts} users={users} />}
+					/>
+					<Route path='posts' element={<Post posts={posts} />} />
+					<Route
+						path='/posts/:postid'
+						element={
+							<PostDetails
+								posts={posts}
+								users={users}
+								handleGetAuthorUsername={
+									handleGetAuthorUsername
+								}
+							/>
+						}
+					/>
+				</Routes>
+			</main>
+			<Footer />
+		</div>
+	);
 }
 
-export default App
+export default App;
