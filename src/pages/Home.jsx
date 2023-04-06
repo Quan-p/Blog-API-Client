1df2c6f3cbd0e4ef8580852a465522f84077cfa2
quan-p <m.phamquan@gmail.com>
2023-04-06 16:28:12 -0500

    Move conditional rendering inside post container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,49 +1,58 @@
-import React, { useEffect, useState } from "react";
-import Hero from "../components/hero/Hero";
-import PostCard from "../components/post_card/PostCard";
-import { ClimbingBoxLoader } from "react-spinners";
+import React, { useEffect, useState } from 'react';
+import Hero from '../components/hero/Hero';
+import PostCard from '../components/post_card/PostCard';
+import { BounceLoader } from 'react-spinners';
 import './home.scss';
 
 const Home = (props) => {
-    const posts = props.posts;
-    const users = props.users;
-    const [recentPosts, setRecentPosts] = useState();
+	const posts = props.posts;
+	const users = props.users;
+	const [recentPosts, setRecentPosts] = useState();
 
-    const override = {
-        position: 'static',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+	const override = {
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+	};
 
-    useEffect(() => {
-        if(posts) {
-            const sortPosts = posts.sort((a, b) =>  new Date(b.date) - new Date(a.date));
-            setRecentPosts(sortPosts.slice(0, 3));
-        }
-    }, [posts]);
+	useEffect(() => {
+		if (posts) {
+			const sortPosts = posts.sort(
+				(a, b) => new Date(b.date) - new Date(a.date)
+			);
+			setRecentPosts(sortPosts.slice(0, 3));
+		}
+	}, [posts]);
 
-    return (
-        <div className="home-container">
-            <Hero />
-            <h3 className="recent-header">Recent Posts</h3>
-            {recentPosts && users ?
-                <div className="post-container">
-                    <ul>
-                        {recentPosts.map(post => {
-                            return <li key={post._id}><PostCard key={post._id} post={post} users={users} /></li>
-                        })}
-                    </ul>
-                    
-                </div> 
-                : <ClimbingBoxLoader
-                    color="#36d7b7"
-                    size={25}
-                    cssOverride={ override }
-                />
-            }
-        </div>
-    );
-}
+	return (
+		<div className='home-container'>
+			<Hero />
+			<h3 className='recent-header'>Recent Posts</h3>
+			<div className='post-container'>
+				{recentPosts && users ? (
+					<ul>
+						{recentPosts.map((post) => {
+							return (
+								<li key={post._id}>
+									<PostCard
+										key={post._id}
+										post={post}
+										users={users}
+									/>
+								</li>
+							);
+						})}
+					</ul>
+				) : (
+					<BounceLoader
+						color='#D4A373'
+						size={45}
+						cssOverride={override}
+					/>
+				)}
+			</div>
+		</div>
+	);
+};
 
 export default Home;
