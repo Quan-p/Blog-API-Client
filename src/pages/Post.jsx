607c7ab86e71be6886f9e45c673b4c506e15c33b
quan-p <m.phamquan@gmail.com>
2023-04-11 17:18:02 -0500

    Update a tag with Link tag to fix client-side rendering

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,44 +1,50 @@
-import React, { useEffect, useState } from "react";
-import { ClimbingBoxLoader } from "react-spinners";
+import React, { useEffect, useState } from 'react';
+import { ClimbingBoxLoader } from 'react-spinners';
+import { Link } from 'react-router-dom';
 import './post.scss';
 
 const Post = (props) => {
-    const posts = props.posts;
-    const [sortedPosts, setSortedPosts] = useState();
+	const posts = props.posts;
+	const [sortedPosts, setSortedPosts] = useState();
 
-    const override = {
-        position: 'static',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+	const override = {
+		position: 'static',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	};
 
-    useEffect(() => {
-        if(posts) {
-            const sortPosts = posts.sort((a, b) =>  new Date(b.date) - new Date(a.date));
-            setSortedPosts(sortPosts);
-        }
-    }, [posts]);
+	useEffect(() => {
+		if (posts) {
+			const sortPosts = posts.sort(
+				(a, b) => new Date(b.date) - new Date(a.date)
+			);
+			setSortedPosts(sortPosts);
+		}
+	}, [posts]);
 
-    return (
-        <div className="posts-container">
-            <h2>All Posts</h2>
-            {sortedPosts ?
-                <ul className="post-list">
-                {sortedPosts.map(post => {
-                    return <a href={`/posts/${post._id}`} key={post._id}>
-                                <li key={post._id}>{post.title}</li>
-                            </a>
-                })}
-                </ul>
-                : <ClimbingBoxLoader
-                color="#36d7b7"
-                size={25}
-                cssOverride={ override }
-            />
-            }
-        </div>
-    );
-}
+	return (
+		<div className='posts-container'>
+			<h2>All Posts</h2>
+			{sortedPosts ? (
+				<ul className='post-list'>
+					{sortedPosts.map((post) => {
+						return (
+							<Link to={`/posts/${post._id}`} key={post._id}>
+								<li key={post._id}>{post.title}</li>
+							</Link>
+						);
+					})}
+				</ul>
+			) : (
+				<ClimbingBoxLoader
+					color='#36d7b7'
+					size={25}
+					cssOverride={override}
+				/>
+			)}
+		</div>
+	);
+};
 
 export default Post;
