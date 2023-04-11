45a3c12c6eebdc77193fed72a897feb58d53fe4e
quan-p <m.phamquan@gmail.com>
2023-04-11 16:55:57 -0500

    Replace a tag with Link

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,23 +1,22 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './hero.scss';
 
 const Hero = () => {
-    return (
-        <div className="hero-container">
-            <h2 className="hero-title">Welcome to my Blog</h2>
-            <h3 className="hero-subtitle">
-                Created as a project for The Odin Project. 
-            </h3>
-            <h3 className="hero-subtitle">
-                Check out my random thoughts and amazing test posts. 
-            </h3>
-            <a href={'/posts'}>
-                <button className="hero-button">     
-                    Start Reading                      
-                </button>
-            </a>
-        </div>
-    )
-}
+	return (
+		<div className='hero-container'>
+			<h2 className='hero-title'>Welcome to my Blog</h2>
+			<h3 className='hero-subtitle'>
+				Created as a project for The Odin Project.
+			</h3>
+			<h3 className='hero-subtitle'>
+				Check out my random thoughts and amazing test posts.
+			</h3>
+			<Link to={'/posts'}>
+				<button className='hero-button'>Start Reading</button>
+			</Link>
+		</div>
+	);
+};
 
 export default Hero;
--- src/components/post_card/PostCard.jsx
+++ src/components/post_card/PostCard.jsx
@@ -1,32 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './postCard.scss';
 
 function PostCard({ post, users }) {
-    const title = post.title;
-    const date = new Date(post.date).toLocaleDateString();
+	const title = post.title;
+	const date = new Date(post.date).toLocaleDateString();
 
-    const authorId = post.author;
-    let author = users.find(user => user._id == authorId);
+	const authorId = post.author;
+	let author = users.find((user) => user._id == authorId);
 
-    if (!author) {
-        console.error(`Author with id "${authorId}" not found`);
-        return null;
-    }
+	if (!author) {
+		console.error(`Author with id "${authorId}" not found`);
+		return null;
+	}
 
-    return (
-    
-        <div className="card-container">
-            <h1 className="author">{author.username}</h1>
-            <h2 className="title">{title}</h2>
-            <p className="date">{date}</p>
-            <a href={`/posts/${post._id}`} key={post._id}>
-                <button className="post-button">
-                    View Post
-                </button>
-            </a>
-        </div>
-        
-    )
+	return (
+		<div className='card-container'>
+			<h1 className='author'>{author.username}</h1>
+			<h2 className='title'>{title}</h2>
+			<p className='date'>{date}</p>
+			<Link to={`/posts/${post._id}`} key={post._id}>
+				<button className='post-button'>View Post</button>
+			</Link>
+		</div>
+	);
 }
 
 export default PostCard;
